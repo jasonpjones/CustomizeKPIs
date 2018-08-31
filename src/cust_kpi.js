54addebd7430bee75f5a23f980b29c1ab6532615
Jason Jones <jason.jones@swiftpage.com>
2018-08-31 14:00:30 -0700

    get some fake data

--- src/cust_kpi.js
+++ src/cust_kpi.js
@@ -1,3 +1,24 @@
-/**
- * Created by jjones on 8/30/2018.
- */
+/* Globals ActInsight*/
+
+var ActInsight = ActInsight || {};
+
+ActInsight.KPICustomizer = function() {
+    this.data = new ActInsight.KPICustomizerData();
+};
+
+ActInsight.KPICustomizer.prototype = {
+
+    initialize: function() {
+        return this.data.initialize()
+            .then(this.loadAvailableAndSelectedLists.bind(this))
+            .then(this.loadFieldPicklist.bind(this));
+    },
+    loadAvailableAndSelectedLists: function() {
+        console.log(this.data.KPIDefinition);
+    },
+    loadFieldPicklist: function() {
+        console.log(this.data.opportunityFields);
+    }
+
+
+};
