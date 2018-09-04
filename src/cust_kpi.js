188892d9abb7a217f66f3d539ca5a96052ef04fc
Jason Jones <jason.jones@swiftpage.com>
2018-09-04 15:19:14 -0700

    add new basics

--- src/cust_kpi.js
+++ src/cust_kpi.js
@@ -10,6 +10,7 @@ ActInsight.KPICustomizer.prototype = {
 
     initialize: function() {
         this._setupButtonHandlers();
+        this._setupTextHandlers();
         return this.data.initialize()
             .then(this.loadAvailableAndSelectedLists.bind(this))
             .then(this.loadFieldPicklist.bind(this));
@@ -37,7 +38,17 @@ ActInsight.KPICustomizer.prototype = {
         }.bind(this));
     },
     loadFieldPicklist: function() {
-        console.log(this.data.opportunityFields);
+        var $fieldSel = $("#create-new-field");
+
+
+        this.data.opportunityFields.forEach(function(oppField) {
+            var  fieldName = oppField.name.replace("customFields/", ""),    //replace designator added by api
+                $optEl = $("<option>", { value: fieldName, text: oppField.displayName  });
+            $fieldSel.append($optEl);
+        });
+     /*   $fieldSel.on("change", this.handleOppFieldChanged.bind(this));
+        $fieldSel.trigger("change"); */
+        $(".custom-select").SumoSelect();
     },
     handleKPIClick: function(e) {
         var $el = $(e.target),
@@ -49,8 +60,45 @@ ActInsight.KPICustomizer.prototype = {
 
         if(!isSelected) {
             $el.addClass("ck-selected");
-            this.updateButtonStatus($el)
+            this.updateButtonStatus($el);
         }
+    },
+    handleAddNewClick: function() {
+        var newName = $("#create-new-name").val().trim(),
+            cleanedName = newName.toLowerCase().replace(" ", "_"),
+            field = $("#create-new-field").val(),
+            operation = $("#create-new-operation").val(),
+            dup = _.find(this.data.KPIDefinition, { name: cleanedName}),
+            $newEl,
+            $avail = $("#available-kpis");
+
+        if(dup) {
+            alert(" _ dup exists msg _");
+            return;
+        }
+
+        this.data.KPIDefinition.push({
+            name: cleanedName,
+            entity: "opportunity",
+            field: field,
+            operation: operation,
+            label: newName,
+            isActive: false,
+            isCustom: true
+        });
+
+        $newEl = $("<div>", {
+            class: "ck-kpi-div",
+            text: newName,
+            "data-kpi-name": cleanedName,
+            "data-isactive": false
+        });
+        $newEl.on("click", this.handleKPIClick.bind(this));
+
+        $avail.append($newEl);
+        $newEl.trigger("click");
+        $("#create-new-name").val("").trigger("change");
+
 
     },
     updateButtonStatus: function($el) {
@@ -122,10 +170,22 @@ ActInsight.KPICustomizer.prototype = {
 
 
     },
+    _setupTextHandlers: function() {
+        $("#create-new-name").on("change", function() {
+            var val =  $("#create-new-name").val();
+            if(val.trim()) {
+                $("#create-new-button").removeAttr("disabled");
+            }
+            else {
+                $("#create-new-button").attr("disabled", "disabled");
+            }
 
+        });
+    },
 
     _setupButtonHandlers: function(){
         $(".ck-button").on("click", this.moveKPI.bind(this));
+        $("#create-new-button").on("click", this.handleAddNewClick.bind(this));
     }
 
 
